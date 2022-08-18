2bb96c197fe15732c36e5ba4bf7b1ab593c2fa2e
Faiqazmi <56787824+Faiqazmi@users.noreply.github.com>
2022-08-18 21:12:36 +0700

    Create main.js

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
